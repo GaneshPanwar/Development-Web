03341d56b547054ca6a9b9c1baf24ed61dd82a15
Ganesh Panwar <panwarg442@gmail.com>
2023-08-26 17:44:26 +0530

    Fibonacci Challenge Completed

--- JavaScript_Challenges/FizzBuzz/index.js
+++ JavaScript_Challenges/FizzBuzz/index.js
@@ -1,3 +1,6 @@
+// Method First Using while loop
+// When something is true (state)
+// like a player is alive
 var output = [];
 var count = 1;
 
@@ -15,5 +18,30 @@ function fizzBuzz() {
 
     count++;
   }
-  console.log(output);
+  return output;
 }
+
+// Method Second Using For Loop 
+// Iterate when you run a piece of code many many times
+// and you're going to use the for loop to define
+// how many times to run 
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  for (var count = 1; count <= 100; count++) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+  }
+  return output;
+}
+
+output = fizzBuzz();
+console.log(output);
