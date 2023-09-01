c00b8ac482d346fc455066933151a51bd63a2e38
Ganesh Panwar <panwarg442@gmail.com>
2023-09-01 09:21:53 +0530

    Calculator Completed

--- JavaScript_Challenges/Calculator/index.js
+++ JavaScript_Challenges/Calculator/index.js
@@ -1,37 +1,54 @@
 
-// Add
-function add(num1, num2) {
-  return num1 + num2;
+// // Add
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// // Subtract
+// function subtract(num1, num2) {
+//   return num1 - num2;
+// }
+
+// // Multiply
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+
+// // Divide
+// function divide(num1, num2) {
+//   return num1 / num2;
+// }
+
+// function calculator(num1, num2 , operator){
+//   return operator(num1, num2);
+// }
+
+// document.querySelector("input.submit").addEventListener("click", submit);
+
+// var input = document.getElementById("num1");
+// var myValue = "ganesh";
+// input.value = myValue;
+// var myFunction = function (e){
+//   console.log(e.value);
+// }
+
+// function submit(){
+//   myFunction();
+// }
+
+function display(val){
+  document.getElementById("result").value +=val;
 }
 
-// Subtract
-function subtract(num1, num2) {
-  return num1 - num2;
+function calculate(){
+  try{
+    document.getElementById("result").value = eval(document.getElementById("result").value);
+  }
+  catch(err){
+    document.getElementById("result").value = "Error";
+  }
 }
 
-// Multiply
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-// Divide
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-function calculator(num1, num2 , operator){
-  return operator(num1, num2);
-}
-
-document.querySelector("input.submit").addEventListener("click", submit);
-
-var input = document.getElementById("num1");
-var myValue = "ganesh";
-input.value = myValue;
-var myFunction = function (e){
-  console.log(e.value);
-}
-
-function submit(){
-  myFunction();
+function clearScreen(){
+  document.getElementById("result").value = "";
 }
