22518b5509a599af56b7501d75c92ceb86fcc652
Ganesh Panwar <panwarg442@gmail.com>
2023-09-01 23:01:31 +0530

    change style

--- JavaScript-Drum_Kit/index.js
+++ JavaScript-Drum_Kit/index.js
@@ -11,21 +11,17 @@
 // document.querySelector("button").addEventListener("click", function (){
 //   alert("I got Clicked");
 //   // What to do when click detected
-// });
+// }); 
 
 
 // Add To All Buttons
 
-// document.querySelectorAll("button")[0].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[1].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[2].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[3].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[4].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[5].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[6].addEventListener("click", handleClick);
-
 function handleClick() {
-  alert("I got Clicked");
+  // alert("I got Clicked");
+  // var audio  = new Audio("sounds/tom-1.mp3"); 
+  // audio.play();
+
+  this.style.color = "white";
 }
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
