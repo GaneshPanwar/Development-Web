7adb65eb2b7ffe1a5eadfb67da1aa82cfd596495
Ganesh Panwar <panwarg442@gmail.com>
2023-09-08 20:53:10 +0530

    working on step -10

--- jQuery-Simon Game/game.js
+++ jQuery-Simon Game/game.js
@@ -4,10 +4,12 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 //5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
 
+
 function nextSequence() {
 
-  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
+  var randomNumber = Math.floor(Math.random() * 4);
+
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -15,4 +17,12 @@ function nextSequence() {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 
+  $(".btn").on("click", function () {
+    
+  });
+
+
 }
+
+nextSequence();
+
