c95b1097d9c1313b87ec0db80ec0c26e0af6720a
Ganesh Panwar <panwarg442@gmail.com>
2023-09-25 17:39:48 +0530

    add https

--- NodeJS/WeatherProject/app.js
+++ NodeJS/WeatherProject/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
+
+https.get("https://api.openweathermap.org/data/2.5/weather",function(res){
+console.log(res.statusCode);
+})
 
 app.get("/", function (req, res) {
   res.send("<h1>Hello</h1>");
