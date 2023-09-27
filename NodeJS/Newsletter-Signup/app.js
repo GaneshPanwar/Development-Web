7149b4f003f653577bbdedbc2e3f8b90a728e006
Ganesh Panwar <panwarg442@gmail.com>
2023-09-27 19:59:02 +0530

    signup form updated

--- NodeJS/Newsletter-Signup/app.js
+++ NodeJS/Newsletter-Signup/app.js
@@ -5,9 +5,10 @@ const request = require("request");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("Hello");
-  
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.listen(3000, function () {
