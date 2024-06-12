e6a4830206240bec60ff9c38b7c88e37a612cc89
Ganesh Panwar <panwarg442@gmail.com>
2024-06-12 06:42:28 +0530

    intro-to-node

---
+++ NodeJS/intro-to-node/index.js
@@ -0,0 +1,8 @@
+import { randomSuperhero } from "superheroes";
+import  {randomSupervillain} from "supervillains";
+
+var mySuperheroName = randomSuperhero();
+var mySuperVillainName = randomSupervillain();
+
+// console.log(mySuperheroName);
+console.log(mySuperVillainName);
